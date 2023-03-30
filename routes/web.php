788450397e68812f1c703468b5dc3d1ce2e4d318
UnesseAh <youness.ahasla@gmail.com>
2023-03-30 14:42:22 +0000

    created AuthController and defined my routed

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController as AuthControllerAlias;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//index,postLogin, registration,postRegistration,dashboard,logout
+
+Route::controller(AuthControllerAlias::class)->group(function(){
+    Route::get('login', 'index')->name('login');
+    Route::post('post-login', 'postLogin')->name('login.post');
+    Route::get('registration', 'registration')->name('register');
+    Route::post('post-registration', 'postRegistration')->name('register.post');
+    Route::get('dashboard', 'dashboard');
+    Route::get('logout','logout')->name('logout');
+});
+
