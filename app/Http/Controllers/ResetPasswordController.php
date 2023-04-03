59e23b802777fe4c423dd249e982d0282d2e5499
UnesseAh <youness.ahasla@gmail.com>
2023-04-03 02:12:45 +0000

    add rest password functin

--- app/Http/Controllers/ResetPasswordController.php
+++ app/Http/Controllers/ResetPasswordController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Str;
 
@@ -40,16 +42,33 @@ class ResetPasswordController extends Controller
 
     public function showResetPasswordForm($token)
     {
-        return view('auth.resetPassword', ['token', $token]);
+        return view('auth.resetPassword', ['token' => $token]);
     }
 
     public function resetPassword(Request $request)
     {
         $request->validate([
-            'email' => 'required',
-            'password' => 'required',
-            'password_confirmation',
+            'email' => 'required|email|exists:users',
+            'password' => 'required|string|min:6|confirmed',
+            'password_confirmation' => 'required',
         ]);
+
+        $foundToken = DB::table('password_resets')->where([
+            'email' => $request->email,
+            'token' => $request->token,
+        ])->first();
+
+        if(!$foundToken)
+        {
+            return back()->withInput()->with('error', 'Invalid token');
+        }
+
+        $user = User::where(['email' => $request->email])
+            ->update(['password' => Hash::make($request->password)]);
+
+        DB::table('password_resets')->where(['email' => $request->email])->delete();
+
+        return redirect('/login')->with('message' , 'Your password has been rest');
     }
 
 }
--- resources/views/auth/resetPassword.blade.php
+++ resources/views/auth/resetPassword.blade.php
@@ -1,3 +1,21 @@
 
+<form action="{{ route('reset.password.submit') }}" method="POST">
+    @csrf
+    <input type="hidden" name="token" value="{{ $token }}">
+    <div>
+        <label for="email">Email :</label>
+        <input name="email" type="email" id="email">
+    </div>
 
-reset password page
+    <div>
+        <label for="password">Password :</label>
+        <input name="password" type="password" id="password">
+    </div>
+
+    <div>
+        <label for="password-confirm">Confirm Password :</label>
+        <input name="password_confirmation" type="password" id="password-confirm">
+    </div>
+
+    <button type="submit">Reset Password</button>
+</form>
