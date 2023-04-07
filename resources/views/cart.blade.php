503259602dfbeca40b50f42e134a4a78a4a79262
UnesseAh <youness.ahasla@gmail.com>
2023-04-07 15:39:35 +0000

    finished the header, footer, and products sections on the landing page

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,86 +1,127 @@
 @include('includes.dashboard.head')
-<h4><i class="fa-solid fa-cart-shopping"></i>My Cart</h4>
-<table class="table align-middle mb-0 bg-white">
-    <thead class="bg-light">
-    <tr>
-        <th>Proudct</th>
-        <th>Price</th>
-        <th>Quantity</th>
-        <th>Total</th>
-        <th>Remove</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr>
-        <td>
-            <div class="d-flex align-items-center">
-                <img src="{{asset('images/product-1.png')}}" alt="" style="width: 45px; height: 45px"/>
-                <div class="ms-3">
-                    <p class="fw-bold mb-1">MOROCCAN KILIM CUSHION, RED</p>
-                </div>
-            </div>
-        </td>
-        <td>
-            <p class="fw-normal mb-1">149DH</p>
-        </td>
-        <td>
-            <span class="badge badge-success rounded-pill d-inline">5</span>
-        </td>
-        <td>298DH</td>
-        <td>
-            <button type="button" class="btn btn-link btn-sm px-3" data-ripple-color="dark">
-                <i class="fas fa-times"></i>
-            </button>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <div class="d-flex align-items-center">
-                <img src="{{asset('images/product-2.png')}}" alt="" style="width: 45px; height: 45px"/>
-                <div class="ms-3">
-                    <p class="fw-bold mb-1">MOROCCAN TRIBAL ENAMELED EARRINGS</p>
-                </div>
-            </div>
-        </td>
-        <td>
-            <p class="fw-normal mb-1">149DH</p>
-        </td>
-        <td>
-            <span class="badge badge-success rounded-pill d-inline">5</span>
-        </td>
-        <td>298DH</td>
-        <td>
-            <button type="button" class="btn btn-link btn-sm px-3" data-ripple-color="dark">
-                <i class="fas fa-times"></i>
-            </button>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <div class="d-flex align-items-center">
-                <img src="{{asset('images/product-3.png')}}" alt="" style="width: 45px; height: 45px"/>
-                <div class="ms-3">
-                    <p class="fw-bold mb-1">MOROCCAN CEILING LIGHTS, ALMINA</p>
-                </div>
-            </div>
-        </td>
-        <td>
-            <p class="fw-normal mb-1">149DH</p>
-        </td>
-        <td>
-            <span class="badge badge-success rounded-pill d-inline">5</span>
-        </td>
-        <td>298DH</td>
-        <td>
-            <button type="button" class="btn btn-link btn-sm px-3" data-ripple-color="dark">
-                <i class="fas fa-times"></i>
-            </button>
-        </td>
-    </tr>
 
-    </tbody>
 
-</table>
-<div class="d-grid gap-2 col-6 mx-auto">
-    <button class="btn btn-primary" type="button">Checkout</button>
+
+<div class="row">
+    <div class="col-md-8 mb-4">
+        <div class="card mb-4">
+            <div class="card-header py-3">
+                <h5 class="mb-0">My Cart</h5>
+            </div>
+            <div class="card-body">
+
+                <table class="table align-middle mb-0 bg-white">
+                    <thead class="bg-light">
+                    <tr>
+                        <th>Proudct</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Total</th>
+                        <th>Remove</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>
+                            <div class="d-flex align-items-center">
+                                <img src="{{asset('images/product-1.png')}}" alt="" style="width: 45px; height: 45px"/>
+                                <div class="ms-3">
+                                    <p class="fw-bold mb-1">MOROCCAN KILIM CUSHION, RED</p>
+                                </div>
+                            </div>
+                        </td>
+                        <td>
+                            <p class="fw-normal mb-1">149DH</p>
+                        </td>
+                        <td>
+                            <span class="badge badge-success rounded-pill d-inline">5</span>
+                        </td>
+                        <td>298DH</td>
+                        <td>
+                            <button type="button" class="btn btn-link btn-sm px-3" data-ripple-color="dark">
+                                <i class="fas fa-times"></i>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div class="d-flex align-items-center">
+                                <img src="{{asset('images/product-2.png')}}" alt="" style="width: 45px; height: 45px"/>
+                                <div class="ms-3">
+                                    <p class="fw-bold mb-1">MOROCCAN TRIBAL ENAMELED EARRINGS</p>
+                                </div>
+                            </div>
+                        </td>
+                        <td>
+                            <p class="fw-normal mb-1">149DH</p>
+                        </td>
+                        <td>
+                            <span class="badge badge-success rounded-pill d-inline">5</span>
+                        </td>
+                        <td>298DH</td>
+                        <td>
+                            <button type="button" class="btn btn-link btn-sm px-3" data-ripple-color="dark">
+                                <i class="fas fa-times"></i>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div class="d-flex align-items-center">
+                                <img src="{{asset('images/product-3.png')}}" alt="" style="width: 45px; height: 45px"/>
+                                <div class="ms-3">
+                                    <p class="fw-bold mb-1">MOROCCAN CEILING LIGHTS, ALMINA</p>
+                                </div>
+                            </div>
+                        </td>
+                        <td>
+                            <p class="fw-normal mb-1">149DH</p>
+                        </td>
+                        <td>
+                            <span class="badge badge-success rounded-pill d-inline">5</span>
+                        </td>
+                        <td>298DH</td>
+                        <td>
+                            <button type="button" class="btn btn-link btn-sm px-3" data-ripple-color="dark">
+                                <i class="fas fa-times"></i>
+                            </button>
+                        </td>
+                    </tr>
+
+                    </tbody>
+
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4 mb-4">
+        <div class="card mb-4">
+            <div class="card-header py-3">
+                <h5 class="mb-0">Summary</h5>
+            </div>
+            <div class="card-body">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
+                        Products
+                        <span>$53.98</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center px-0">
+                        Shipping
+                        <span>Gratis</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
+                        <div>
+                            <strong>Subtotal</strong>
+                        </div>
+                        <span><strong>$53.98</strong></span>
+                    </li>
+                </ul>
+
+                <a type="button" class="btn btn-primary btn-lg btn-block">
+                    Checkout
+                </a>
+            </div>
+        </div>
+    </div>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,24 +15,59 @@
 </head>
 <body>
 <!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark" >
     <!-- Container wrapper -->
     <div class="container">
         <!-- Navbar brand -->
+        <a class="navbar-brand me-2" href="https://mdbgo.com/">
+            <img src="{{ asset('images/logo/icon/al-artisan-icon-white.png') }}" height="36" alt="MDB Logo" loading="lazy" style="margin-top: -1px;"/>
+        </a>
 
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
-            </li>
-        </ul>
+        <!-- Toggle button -->
+        <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarButtonsExample" aria-controls="navbarButtonsExample" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fas fa-bars"></i>
+        </button>
+
+        <!-- Collapsible wrapper -->
+        <div class="collapse navbar-collapse" id="navbarButtonsExample">
             <!-- Left links -->
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Artisans</a>
+                </li>
+                <!-- Navbar dropdown -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-mdb-toggle="dropdown" aria-expanded="false">Categories</a>
+                    <!-- Dropdown menu -->
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li>
+                            <a class="dropdown-item" href="#">Lighting</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#">Rugs</a>
+                        </li>
+                        <li>
+                    </ul>
+                </li>
+                <!-- Navbar dropdown -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-mdb-toggle="dropdown" aria-expanded="false">Info</a>
+                    <!-- Dropdown menu -->
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li>
+                            <a class="dropdown-item" href="#">About Us</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#">FAQ</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#">Contact</a>
+                        </li>
 
+                    </ul>
+                </li>
+            </ul>
+            <!-- Left links -->
             <div class="d-flex align-items-center">
                 <button type="button" class="btn btn-link px-3 me-2">
                     Login
@@ -49,6 +84,287 @@
 </nav>
 <!-- Navbar -->
 
+<div class="container">
+    <div class="row row-cols-1 row-cols-md-4 g-4">
+        <div class="col">
+            <div class="card">
+                <img src="{{asset('images/product-1.png')}}" class="card-img-top" alt="Hollywood Sign on The Hill"/>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                        This is a longer card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="{{asset('images/product-1.png')}}" class="card-img-top" alt="Palm Springs Road"/>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                        This is a longer card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="{{asset('images/product-1.png')}}" class="card-img-top" alt="Los Angeles Skyscrapers"/>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="{{asset('images/product-1.png')}}" class="card-img-top" alt="Skyscrapers"/>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                        This is a longer card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="{{asset('images/product-1.png')}}" class="card-img-top" alt="Hollywood Sign on The Hill"/>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                        This is a longer card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="{{asset('images/product-1.png')}}" class="card-img-top" alt="Palm Springs Road"/>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                        This is a longer card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="{{asset('images/product-1.png')}}" class="card-img-top" alt="Los Angeles Skyscrapers"/>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="{{asset('images/product-1.png')}}" class="card-img-top" alt="Skyscrapers"/>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                        This is a longer card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Footer -->
+<footer class="bg-dark text-center text-white">
+    <!-- Grid container -->
+    <div class="container p-4">
+        <!-- Section: Social media -->
+        <section class="mb-4">
+            <!-- Facebook -->
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+                <i class="fab fa-facebook-f"></i>
+            </a>
+
+            <!-- Twitter -->
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+                <i class="fab fa-twitter"></i>
+            </a>
+
+            <!-- Google -->
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+                <i class="fab fa-google"></i>
+            </a>
+
+            <!-- Instagram -->
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+                <i class="fab fa-instagram"></i>
+            </a>
+
+            <!-- Linkedin -->
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+                <i class="fab fa-linkedin-in"></i>
+            </a>
+
+            <!-- Github -->
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+                <i class="fab fa-github"></i>
+            </a>
+        </section>
+        <!-- Section: Social media -->
+
+        <!-- Section: Form -->
+        <section class="">
+            <form action="">
+                <!--Grid row-->
+                <div class="row d-flex justify-content-center">
+                    <!--Grid column-->
+                    <div class="col-auto">
+                        <p class="pt-2">
+                            <strong>Sign up for our newsletter</strong>
+                        </p>
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-md-5 col-12">
+                        <!-- Email input -->
+                        <div class="form-outline form-white mb-4">
+                            <input type="email" id="form5Example21" class="form-control" />
+                            <label class="form-label" for="form5Example21">Email address</label>
+                        </div>
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-auto">
+                        <!-- Submit button -->
+                        <button type="submit" class="btn btn-outline-light mb-4">
+                            Subscribe
+                        </button>
+                    </div>
+                    <!--Grid column-->
+                </div>
+                <!--Grid row-->
+            </form>
+        </section>
+        <!-- Section: Form -->
+
+        <!-- Section: Text -->
+        <section class="mb-4">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum
+                repellat quaerat voluptatibus placeat nam, commodi optio pariatur est quia magnam
+                eum harum corrupti dicta, aliquam sequi voluptate quas.
+            </p>
+        </section>
+        <!-- Section: Text -->
+
+        <!-- Section: Links -->
+        <section class="">
+            <!--Grid row-->
+            <div class="row">
+                <!--Grid column-->
+                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">Links</h5>
+                    <ul class="list-unstyled mb-0">
+                        <li>
+                            <a href="#!" class="text-white">Link 1</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Link 2</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Link 3</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Link 4</a>
+                        </li>
+                    </ul>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">Links</h5>
+
+                    <ul class="list-unstyled mb-0">
+                        <li>
+                            <a href="#!" class="text-white">Link 1</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Link 2</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Link 3</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Link 4</a>
+                        </li>
+                    </ul>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">Links</h5>
+
+                    <ul class="list-unstyled mb-0">
+                        <li>
+                            <a href="#!" class="text-white">Link 1</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Link 2</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Link 3</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Link 4</a>
+                        </li>
+                    </ul>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">Links</h5>
+
+                    <ul class="list-unstyled mb-0">
+                        <li>
+                            <a href="#!" class="text-white">Link 1</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Link 2</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Link 3</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Link 4</a>
+                        </li>
+                    </ul>
+                </div>
+                <!--Grid column-->
+            </div>
+            <!--Grid row-->
+        </section>
+        <!-- Section: Links -->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2020 Copyright:
+        <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    </div>
+    <!-- Copyright -->
+</footer>
+<!-- Footer -->
 <!-- MDB -->
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
